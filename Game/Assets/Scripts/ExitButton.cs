853632080bea93c2ab2b4a5219a8a1cddaa9edd2
I-Akinori <rhaarle1140@gmail.com>
2020-03-09 10:09:40 +0900

    UI実装/Exitボタン追加

---
+++ Game/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ExitButton : MonoBehaviour
+{
+    void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+    }
+    public void OnClick()
+    {
+        Quit();
+    }
+}
